1da9d090753fa4e9c6f42dcf7fa2bd74cc88b272
patriceTchuente <tchuentekammognepatrice@gmail.com>
2022-01-05 10:11:20 +0100

    Exo3 resolut

--- TP1/Exercice3.dart
+++ TP1/Exercice3.dart
@@ -1,1 +1,16 @@
-/* Écrire une fonction qui permet de connaître la taille d'un tableau à partir de la plage d'indices valides*/  
+/* Écrire une fonction qui permet de connaître la taille d'un tableau à partir de la plage d'indices valides*/
+
+void main() {
+  //test
+  var tableau = [12, 43, 'juce', 12.4];
+  print(tailleTab(tableau));
+}
+
+int tailleTab(List tab) {
+  // Remarque: en Dart on a la deja une fonction qui nous permet de connaitre la taille d'un tableau: length
+  int compteur = 0;
+  for (var elmt in tab) {
+    compteur++;
+  }
+  return compteur;
+}
