b85e75fe218601c7901ccaf485ced6e63ec1bf1c
patriceTchuente <tchuentekammognepatrice@gmail.com>
2022-01-14 18:55:18 +0100

    fin du tp sur les arbres

--- TP2-ABR/ABR.dart
+++ TP2-ABR/ABR.dart
@@ -6,13 +6,22 @@ void main(List<String> args) {
   ABR arbre = new ABR(1);
 
   arbre.noeud!.setFilsgauche = new Noeud(2);
+  arbre.noeud!.fils_gauche!.setFilsdroit = new Noeud(32);
+  arbre.noeud!.fils_gauche!.setFilsgauche = new Noeud(320);
   arbre.noeud!.setFilsdroit = new Noeud(3);
 
-  arbre.noeud!.fils_gauche!.setFilsgauche = new Noeud(4);
+  //arbre.noeud!.fils_gauche!.setFilsgauche = new Noeud(4);
   arbre.noeud!.fils_gauche!.fils_gauche!.setFilsgauche = new Noeud(9);
+
   print(arbre.getNoeud!.fils_gauche!.getCle);
-  int s = Noeud.hauteurABR(arbre.noeud);
+  int hauteur = Noeud.hauteurArbre(arbre.noeud);
+  int nbreNoeud = Noeud.nombreNoeudArbre(arbre.noeud);
+  print("hauteur: ${hauteur} nombre de noeud: ${nbreNoeud}");
+  int s = Noeud.nombreDeFeuille(arbre.noeud);
+  int s1 = Noeud.nombreDeNoeudInterne(arbre.noeud);
   print(s);
+  print(s1);
+
 }
 
 class ABR {
--- TP2-ABR/Noeud.dart
+++ TP2-ABR/Noeud.dart
@@ -20,21 +20,105 @@ class Noeud {
   Noeud? get getFilsdroit => this.fils_droit;
   set setFilsdroit(Noeud value) => this.fils_droit = value;
 
+  /*--------------- Fonction necessaire pour la suite du travail --------------- */
 
-  /* Exo 1
+  static bool estVide(Noeud? n) {
+    return n == null;
+  }
+
+  static bool estUneFeuille(Noeud? n) {
+    if (estVide(n)) {
+      return false;
+    } else if ((estVide(n!.fils_gauche)) && (estVide(n.fils_droit))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static bool estNoeudInterne(Noeud? n) {
+    return !estUneFeuille(n);
+  }
+
+  /* ------------------------- Exo 1-----------------------------
     Calculer la hauteur d'un arbre en vous basant sur la définition récursive :
-
-    - un arbre vide est de hauteur 0 ;
-    - un arbre non vide a pour hauteur 1 + la hauteur maximale entre ses fils.
-*/
-  static int hauteurABR(Noeud? abr) {
-    if (abr == null) {
+      - un arbre vide est de hauteur 0 ;
+      - un arbre non vide a pour hauteur 1 + la hauteur maximale entre ses fils.
+  */
+  static int hauteurArbre(Noeud? abr) {
+    if (estVide(abr)) {
       return 0;
     } else {
-      int t1 = hauteurABR(abr.fils_gauche);
-      int t2 = hauteurABR((abr.fils_droit));
-      int au = 1 + max(t1, t2);
-      return au;
+      int t1 = hauteurArbre(abr!.fils_gauche);
+      int t2 = hauteurArbre(abr.fils_droit);
+      return 1 + max(t1, t2);
+    }
+  }
+
+  /* ----------------------- Exo 2 -------------------------------
+  
+    Calculer le nombre de nœuds en vous basant sur la définition récursive :
+      - si l'arbre est vide : renvoyer 0 ;
+      - sinon renvoyer 1 plus la somme du nombre de nœuds des sous-arbres.
+  */
+  static int nombreNoeudArbre(Noeud? abr) {
+    if (estVide(abr)) {
+      return 0;
+    } else {
+      int compteur1 = nombreNoeudArbre(abr!.fils_gauche);
+      int compteur2 = nombreNoeudArbre(abr.fils_droit);
+      return 1 + compteur1 + compteur2;
+    }
+  }
+  /*-------------------------- Exo 3 ---------------------------------
+  Calculer le nombre de feuilles en vous basant sur la définition récursive :
+    un arbre vide n'a pas de feuille ;
+
+    un arbre non vide a son nombre de feuilles défini de la façon suivante :
+    si le nœud est une feuille, alors on renvoie 1,
+    si c'est un nœud interne, alors le nombre de feuilles est la somme du nombre de feuilles de chacun de ses fils.
+  */
+
+  static int nombreDeFeuille(Noeud? n) {
+    //une feuille est un noeud qui n'a pas d'enfant
+    if (estVide(n)) {
+      return 0;
+    } else if (estUneFeuille(n)) {
+      return 1;
+    } else {
+      return nombreDeFeuille(n!.fils_gauche) + nombreDeFeuille(n.fils_droit);
+    }
+  }
+
+  /*-------------------------- Exo 4 ---------------------------------
+  Calculer le nombre de nœuds internes en vous basant sur la définition récursive :
+    un arbre vide n'a pas de nœud interne.
+    si le nœud en cours n'a pas de fils alors renvoyer 0
+    si le nœud a au moins un fils, renvoyer 1 plus la somme des nœuds interne des sous-arbres.
+
+  */
+
+  static int nombreDeNoeudInterne(Noeud? n) {
+    //un noeud interne est un noeud qui a au moins un enfant
+    if (estVide(n)) {
+      return 0;
+    } else if (estUneFeuille(n)) {
+      return 0;
+    } else {
+      return 1 +
+          nombreDeNoeudInterne(n!.fils_gauche) +
+          nombreDeNoeudInterne(n.fils_droit);
+    }
+  }
+
+  /*------------------------------- Exo 5 ---------------------
+    Créer un algorithme permettant de détruire complètement un arbre.
+  */
+  void supprimerArbre(Noeud? abr) {
+    if (!estVide(abr)) {
+      supprimerArbre(abr!.fils_gauche);
+      supprimerArbre(abr.fils_droit);
+      abr.cle = null;
     }
   }
 }
